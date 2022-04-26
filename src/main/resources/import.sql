7511783830c2f1b44b5ce7ff576167d3e4bff2cc
Joaquim Pessoa Filho <joaquimp@gmail.com>
2022-04-26 08:23:42 -0300

    Removido arquivo .sql e removida as propriedades de banco do projeto

--- src/main/resources/import.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO customers(name, email) VALUES ('Bob Johnson', 'bob@email.com');
-INSERT INTO customers(name, email) VALUES ('Randy Smith', 'randy@email.com');
-INSERT INTO customers(name, email) VALUES ('Sue Rogers', 'sue@email.com');
-
-INSERT INTO items(name, description, price) VALUES ('widget', 'Does a thing', 5.00);
-
-COMMIT;
